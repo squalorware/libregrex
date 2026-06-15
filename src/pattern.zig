0a9f2b4fca8071b50955506995f0f8a4091e7887
oniko94 <onikolaiev94@outlook.com>
2026-06-15 09:16:56 +0300

    Add tests and documentation for Pattern

--- src/pattern.zig
+++ src/pattern.zig
@@ -1,9 +1,25 @@
+//! Compiled regular expression pattern representation.
+//! 
+//! Defines a public `Pattern` exported by Regrex API. `Pattern` is 
+//! an opaque type which encapsulates a heap-allocated `CompilePattern` object
+//! which stores the compiled bytecode instructions, a number of capturing groups 
+//! and the original pattern string.
+//! 
+//! `CompiledPattern` is intentionally hidden behind `Pattern` to prevent manipulations
+//! with the compiled bytecode. `Pattern` exposes a set of methods that wrap over respective
+//! VM functions, allowing end user interaction without exposing the bytecode directly.
 const std = @import("std");
 const Instruction = @import("./core/icr.zig").Instruction;
 const Match = @import("./Match.zig");
 const VM = @import("vm.zig");
 
-pub fn freeBytecode(
+/// Frees compiled bytecode buffer and any owned data
+/// stored inside instructions.
+/// 
+/// Character class instructions own duplicated 
+/// `ranges` and `chars` slices and must be released
+/// before the bytecode slice itself is freed.
+pub fn freeCompiledBuffer(
     alloc: std.mem.Allocator, 
     bytecode: []Instruction
 ) void {
@@ -20,14 +36,28 @@ pub fn freeBytecode(
     alloc.free(bytecode);
 }
 
+/// Internal representation of a compiled regular expression pattern.
+/// 
+/// It is deliberatly unavailable from outside to prevent any malicious access.
+/// Users can interact with it only through opaque top-level `Pattern` type. 
 const CompiledPattern = struct {
+    /// Allocator that owns thsi compiled pattern and its bytecode
     alloc: std.mem.Allocator,
+    /// Bytecode instructions buffer
     bytecode: []Instruction,
+    /// Number of groups returned by the pattern, excluding group 0
     group_count: usize,
+    /// Original pattern string (borrowed, not owned by allocator)
     pattern: []const u8,
 };
 
+/// Opaque type to secure the internal CompiledPattern representation.
+/// 
+/// Ensures full encapsulation and exposes outside only a specific set of operations
+/// Withour giving access to the compiled bytecode buffer itself
 pub const Pattern = opaque {
+    /// Consumes bytecode produced by compiler taking ownership over it
+    /// and creates an opaque public handle for internal `CompiledPattern` representation.
     pub fn init(
         alloc: std.mem.Allocator,
         group_count: usize,
@@ -44,18 +74,30 @@ pub const Pattern = opaque {
         return @ptrCast(self);
     }
 
+    /// Destroys the compiled pattern and releases the bytecode buffer.
+    /// 
+    /// The pointer must be created by the `Pattern.init` using the same allocator
+    /// which is stored in the internal `CompiledPattern` object.
     pub fn deinit(ptr: *Pattern) void {
         const self: *CompiledPattern = @ptrCast(@alignCast(ptr));
         const alloc = self.alloc;
 
-        freeBytecode(alloc, self.bytecode);
+        freeCompiledBuffer(alloc, self.bytecode);
 
         self.* = undefined;
         alloc.destroy(self);
     }
 
+    /// Scans `input` string and returns the first Match 
+    /// of this pattern it has encountered at any position.
+    /// 
+    /// Returns a `Match` object on success
+    /// 
+    /// Returns `null` if no match found
+    /// 
+    /// Returns `Error` if allocation failed or encountered invalid Unicode 
     pub fn search(ptr: *const Pattern, input: []const u8) !?Match {
-        const self: *CompiledPattern = @ptrCast(@alignCast(ptr));
+        const self: *const CompiledPattern = @ptrCast(@alignCast(ptr));
         return VM.search(
             self.alloc,
             self.bytecode,
@@ -63,8 +105,16 @@ pub const Pattern = opaque {
             input,
         );
     }
+
+    /// Matches this pattern against the beginning of the `input` string.
+    /// 
+    /// Returns a `Match` object if a Match was found at the `input` start
+    /// 
+    /// Returns `null` if no match found or match was not at the beginning.
+    /// 
+    /// Returns `Error` if allocation failed or encountered invalid Unicode 
     pub fn match(ptr: *const Pattern, input: []const u8) !?Match {
-        const self: *CompiledPattern = @ptrCast(@alignCast(ptr));
+        const self: *const CompiledPattern = @ptrCast(@alignCast(ptr));
         return VM.match(
             self.alloc,
             self.bytecode,
@@ -72,8 +122,17 @@ pub const Pattern = opaque {
             input,
         );
     }
+
+    /// Retrieves all non-overlapping matches of this pattern found 
+    /// in the `input` string.
+    /// 
+    /// Returns a slice of `Match` objects in case of success. 
+    /// This slice is allocator-owned and must be released; `Match` items
+    /// it contains may also own captured data and must be released individually.
+    /// 
+    /// Returns `Error` if allocation failed or if VM encountered an unrecoverable error.
     pub fn findAll(ptr: *const Pattern, input: []const u8) ![]Match {
-        const self: *CompiledPattern = @ptrCast(@alignCast(ptr));
+        const self: *const CompiledPattern = @ptrCast(@alignCast(ptr));
         return VM.findAll(
             self.alloc,
             self.bytecode,
@@ -82,3 +141,108 @@ pub const Pattern = opaque {
         );
     }
 };
+
+const testing = std.testing;
+
+/// Returns a fixture providing an allocator-owned buffer of bytecode instructions.
+fn bytecodeFixture(alloc: std.mem.Allocator) ![]Instruction {
+    return try alloc.dupe(Instruction, &[_]Instruction {
+        .{ .Save = 0 },
+        .{ .Rune = '4' },
+        .{ .Rune = '2' },
+        .{ .Rune = '0' },
+        .{ .Save = 1 },
+        .Match,
+    });
+}
+
+test "Should return a `Match` from input start by Pattern.match" {
+    const allocator = testing.allocator;
+    const bytecode = try bytecodeFixture(allocator);
+
+    const pattern: *Pattern = try Pattern.init(
+        allocator,
+        0,
+        bytecode,
+        "420"
+    );
+    defer pattern.deinit();
+
+    var result = (try pattern.match("420 kek")) orelse {
+        try testing.expect(false);
+        return;
+    };
+    defer result.deinit(allocator);
+
+    try testing.expectEqualStrings("420", result.str());
+    try testing.expectEqual(@as(usize, 0), result.span.start);
+    try testing.expectEqual(@as(usize, 3), result.span.end);
+}
+
+test "Should return `null` when no match at input start by Pattern.match" {
+    const allocator = testing.allocator;
+    const bytecode = try bytecodeFixture(allocator);
+
+    const pattern: *Pattern = try Pattern.init(
+        allocator,
+        0,
+        bytecode,
+        "420",
+    );
+    defer pattern.deinit();
+
+    const result = try pattern.match("lol 420 kek");
+
+    try testing.expect(result == null);
+}
+
+test "Should return first Match from input by Pattern.search" {
+    const allocator = testing.allocator;
+    const bytecode = try bytecodeFixture(allocator);
+
+    const pattern: *Pattern = try Pattern.init(
+        allocator,
+        0,
+        bytecode,
+        "420",
+    );
+    defer pattern.deinit();
+
+    var result = (try pattern.search("lol 420 kek")) orelse {
+        try testing.expect(false);
+        return;
+    };
+
+    try testing.expectEqualStrings("420", result.str());
+    try testing.expectEqual(@as(usize, 4), result.span.start);
+    try testing.expectEqual(@as(usize, 7), result.span.end);
+}
+
+test "Should return all non-overlapping matches by Pattern.findAll" {
+    const allocator = testing.allocator;
+    const bytecode = try bytecodeFixture(allocator);
+    const expected_matches = [_]struct {
+        start: usize,
+        end: usize,
+    }{
+        .{ .start = 0, .end = 3 },
+        .{ .start = 8, .end = 11 },
+    };
+
+    const pattern: *Pattern = try Pattern.init(
+        allocator,
+        0,
+        bytecode,
+        "420"
+    );
+    defer pattern.deinit();
+
+    const results = try pattern.findAll("420 lol 420 kek");
+    defer Match.free(allocator, results);
+
+    for (expected_matches, 0..) |expected, i| {
+        try testing.expectEqualStrings("420", results[i].str());
+        try testing.expectEqual(expected.start, results[i].span.start);
+        try testing.expectEqual(expected.end, results[i].span.end);
+    }
+}
