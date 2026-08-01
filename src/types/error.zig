54f6a433bc05b578c91409fe53cfb16a24434d0f
oniko94 <onikolaiev94@outlook.com>
2026-08-01 03:34:45 +0300

    WIP: move errors to new type module

---
+++ src/types/error.zig
@@ -0,0 +1,29 @@
+//! Shared error set for the regex frontend and compiler.
+
+/// Common parsing and compilation errors
+pub const Error = error {
+    InvalidArgument,
+    NoMatch,
+    /// Group index is outside of range
+    InvalidGroupIndex,
+    /// Exceeded maximum group count limit
+    GroupBufferOverflow,
+    /// Expected an expression; found an empty branch/sequence.
+    ExpressionExpected,
+    /// Invalid use of repetition operator `*`
+    InvalidRepeat,
+    /// Out of memory
+    MemoryError,
+    /// An invalid or broken UTF-8 character
+    InvalidUnicode,
+    /// Trailing backslash at the pattern end
+    TrailingEscape,
+    /// Token invalid in current context
+    UnexpectedToken,
+    /// Unexpected end of pattern (EOF before construct complete)
+    UnexpectedEnd,
+    /// Missing `)`
+    UnmatchedParen,
+    /// Missing `]`
+    UnmatchedBracket,
+};
