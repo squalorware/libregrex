e6d59ea92808ee06c8270447f75a3d2b8abce27f
oniko94 <onikolaiev94@outlook.com>
2026-08-02 15:31:47 +0300

    WIP: add root file for types module

---
+++ src/types/root.zig
@@ -0,0 +1,9 @@
+const managed = @import("./managed.zig");
+const matching = @import("./matching.zig");
+
+pub const Error = @import("./error.zig").Error;
+pub const ManagedArrayList = managed.ManagedArrayList;
+pub const ManagedOpaqueWrapper = managed.ManagedOpaqueWrapper;
+pub const Match = matching.Match;
+pub const MatchList = matching.MatchList;
+pub const Span = matching.Span;
