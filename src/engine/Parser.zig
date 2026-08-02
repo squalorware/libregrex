596199097e837e576285a69cf7ee258c4fedef76
oniko94 <onikolaiev94@outlook.com>
2026-08-02 19:04:36 +0300

    WIP: rename ast module to syntax; refactor syntax and Parser

---
+++ src/engine/Parser.zig
@@ -0,0 +1,363 @@
+const std = @import("std");
+const types = @import("types");
+const Rune = @import("unicode").Rune;
+const AST = @import("./syntax.zig");
+const Lexer = @import("./Lexer.zig");
+const tokens = @import("./tokens.zig");
+const RegrexError = types.Error;
+const Token = tokens.Token;
+const TokenType = tokens.TokenType;
+
+/// Parser state instance for a single token stream
+pub const Self = @This();
+
+/// Controls AST lifetime.
+alloc: std.mem.Allocator,
+group_count: usize = 0,
+pos: usize = 0,
+/// Borrowed slice representing lexical token stream
+token_list: []const Token,
+
+/// `alloc`: controls AST nodes and owned child slices. 
+/// 
+/// Prefer an `ArenaAllocator`and release the whole 
+/// AST after compiling to bytecode
+pub fn init(
+    alloc: std.mem.Allocator,
+    tlist: []const Token,
+) Self {
+    return .{
+        .alloc = alloc,
+        .token_list = tlist,
+    };
+}
+
+/// Returns a token at the current 'cursor' position
+fn current(self: Self) Token {
+    return self.token_list[self.pos];
+}
+
+/// Returns a token at `pos + offset` or `null` 
+/// if index out of range
+fn peek(self: Self, offset: usize) ?Token {
+    const idx = self.pos + offset;
+
+    if (idx >= self.token_list.len) {
+        return null;
+    }
+
+    return self.token_list[idx];
+}
+
+/// Returns the current token and moves one position 'forward'
+fn advance(self: *Self) Token {
+    const token = self.current();
+    self.pos += 1;
+    return token;
+}
+
+/// Takes `TokenType` and checks if current token 
+/// has matching type
+fn match(self: *Self, typ: TokenType) bool {
+    if (self.current().typ == typ) {
+        _ = self.advance();
+        return true;
+    }
+    return false;
+}
+
+/// Takes `TokenType` and checks if current token 
+/// has matching type.
+/// 
+/// Returns `Error.UnexpectedToken` if type mismatch -
+/// current token doesn't match context
+fn expect(self: *Self, typ: TokenType) RegrexError!Token {
+    if (self.current().typ != typ) {
+        return RegrexError.UnexpectedToken;
+    }
+    return self.advance();
+}
+
+/// Allocates and initializes an AST Node
+fn createNode(self: *Self, node: AST.Node) RegrexError!*AST.Node {
+    const ptr = self.alloc.create(AST.Node) catch {
+        return RegrexError.MemoryError;
+    };
+    ptr.* = node;
+    return ptr;
+}
+
+/// Parses branching.
+/// 
+/// Alteration has the lowest precedence in this grammar
+fn parseBranch(self: *Self) RegrexError!*AST.Node {
+    var left = try self.parseSequence();
+
+    while (self.match(.PIPE)) {
+        const right = try self.parseSequence();
+        left = try self.createNode(.{
+            .Branch = .{
+                .left = left,
+                .right = right,
+            },
+        });
+    }
+    return left;
+}
+
+/// Parses a sequence of quantified Atoms until `EOF`, `RPAREN` or `PIPE`
+fn parseSequence(self: *Self) RegrexError!*AST.Node {
+    var nodes = std.ArrayList(*AST.Node).empty;
+    errdefer nodes.deinit(self.alloc);
+
+    while(
+        self.current().typ != .EOF and
+        self.current().typ != .RPAREN and
+        self.current().typ != .PIPE
+    ) {
+        const node = try self.parseQuantifier();
+        nodes.append(self.alloc, node) catch {
+            return RegrexError.MemoryError;
+        };
+    }
+
+    if (nodes.items.len == 0) {
+        return RegrexError.ExpressionExpected;
+    }
+
+    if (nodes.items.len == 1) {
+        const only = nodes.items[0];
+        nodes.deinit(self.alloc);
+        return only;
+    }
+
+    const owned = nodes.toOwnedSlice(self.alloc) catch {
+        return RegrexError.MemoryError;
+    };
+
+    return self.createNode(.{
+        .Sequence = .{
+            .nodes = owned,
+        },
+    });
+}
+
+/// Parses an Atom and an optional postfix quantifier (`*`, `+` or `?`)
+fn parseQuantifier(self: *Self) RegrexError!*AST.Node {
+    const node = try self.parseAtom();
+
+    // Parse 'zero or more'
+    if (self.match(.STAR)) {
+        return self.createNode(.{
+            .Repeat = .{
+                .node = node,
+                .min = 0,
+                .max = null,
+            },
+        });
+    }
+
+    // Parse 'one or more'
+    if (self.match(.PLUS)) {
+        return self.createNode(.{
+            .Repeat = .{
+                .node = node,
+                .min = 1,
+                .max = null,
+            },
+        });
+    }
+
+    // Parse 'zero or one'
+    if (self.match(.QUESTION)) {
+        return self.createNode(.{
+            .Repeat = .{
+                .node = node,
+                .min = 0,
+                .max = 1,
+            },
+        });
+    }
+    return node;
+}
+
+/// Parses the base indivisible expression
+fn parseAtom(self: *Self) RegrexError!*AST.Node {
+    const token = self.current();
+
+    switch (token.typ) {
+        .CHAR, .ESCAPED_CHAR => {
+            _ = self.advance();
+            return self.createNode(.{
+                .Literal = .{
+                    .value = token.val.?.raw(),
+                },
+            });
+        },
+        .DOT => {
+            _ = self.advance();
+            return self.createNode(.{ .AnyChar = .{} });
+        },
+        .CARET => {
+            _ = self.advance();
+            return self.createNode(.{ .StartAnchor = .{} });            
+        },
+        .DOLLAR => {
+            _ = self.advance();
+            return self.createNode(.{ .EndAnchor = .{} });    
+        },
+        .LPAREN => {
+            _ = self.advance();
+            return self.parseGroup();
+        },
+        .LBRACKET => {
+            _ = self.advance();
+            const class = try self.parseCharClass();
+
+            return self.createNode(.{
+                .CharClass = class,
+            });
+        },
+        else => return RegrexError.UnexpectedToken,
+    }
+}
+
+/// Parses a capturing `(...)` or non-capturing `(?:...)` group
+fn parseGroup(self: *Self) RegrexError!*AST.Node {
+    const first = self.peek(0);
+    const next = self.peek(1);
+
+    // Parse a non-capturing group
+    if (
+        first != null and 
+        next != null and
+        first.?.typ == .QUESTION and
+        next.?.typ == .CHAR and
+        next.?.val.?.raw() == ':'
+    ) {
+        _ = self.advance(); // QUESTION
+        _ = self.advance(); // CHAR ':'
+
+        const node= try self.parseBranch();
+
+        if (!self.match(.RPAREN)) {
+            return RegrexError.UnmatchedParen;
+        }
+
+        return self.createNode(.{
+            .NonCaptureGroup = .{
+                .node = node,
+            },
+        });
+    }
+    // Parse a capturing group
+    self.group_count += 1;
+    const pos = self.group_count;
+
+    const node = try self.parseBranch();
+
+    if (!self.match(.RPAREN)) {
+        return RegrexError.UnmatchedParen;
+    }
+
+    return self.createNode(.{
+        .CaptureGroup = .{
+            .pos = pos,
+            .node = node,
+        },
+    });
+}
+
+/// Parses a character class after the opening `LBRACKET`
+/// 
+/// Supports:
+/// - explicit characters
+/// - inclusive ranges (e.g. `a-z`, `0-9`)
+/// - escaped class members (e.g. `\*`)
+/// - leading negation (`^`)
+fn parseCharClass(self: *Self) RegrexError!AST.CharClass {
+    const negated = self.match(.CARET);
+
+    var ranges = std.ArrayList(AST.CharRange).empty;
+    errdefer ranges.deinit(self.alloc);
+
+    var chars = std.ArrayList(Rune).empty;
+    errdefer chars.deinit(self.alloc);
+
+    while (
+        self.current().typ != .RBRACKET and 
+        self.current().typ != .EOF
+    ) {
+        const start_token = self.current();
+
+        if (
+            start_token.typ != .CHAR and 
+            start_token.typ != .ESCAPED_CHAR
+        ) {
+            return RegrexError.UnexpectedToken;
+        }
+
+        _ = self.advance();
+        const start = start_token.val.?.raw();
+
+        if (self.match(.DASH)) {
+            const end_token = self.current();
+
+            if (end_token.typ == .RBRACKET) {
+                chars.append(self.alloc, start) catch {
+                    return RegrexError.MemoryError;
+                };
+                chars.append(self.alloc, '-') catch {
+                    return RegrexError.MemoryError;
+                };
+                break;
+            }
+
+            if (
+                end_token.typ != .CHAR and
+                end_token.typ != .ESCAPED_CHAR
+            ) {
+                return RegrexError.UnexpectedToken;
+            }
+            _ = self.advance();
+            ranges.append(self.alloc, .{
+                .start = start,
+                .end = end_token.val.?.raw(),
+            }) catch {
+                return RegrexError.MemoryError;
+            };
+        } else {
+            chars.append(self.alloc, start) catch {
+                return RegrexError.MemoryError;
+            };
+        }
+    }
+    if (!self.match(.RBRACKET)) {
+        return RegrexError.UnmatchedBracket;
+    }
+    return .{
+        .ranges = ranges.toOwnedSlice(self.alloc) catch {
+            return RegrexError.MemoryError;
+        },
+        .chars = chars.toOwnedSlice(self.alloc) catch {
+            return RegrexError.MemoryError;
+        },
+        .negated = negated,
+    };
+}
+
+/// Top-level callable.
+/// 
+/// Parses the whole `Token` stream and returns the whole AST
+/// starting with root Node.
+/// 
+/// Returns `RegrexError.UnexpectedToken` if the `Token` stream
+/// does not end with `EOF`
+pub fn parse(self: *Self) RegrexError!*AST.Node {
+    const ast = try self.parseBranch();
+
+    if (self.current().typ != .EOF) {
+        return RegrexError.UnexpectedToken;
+    }
+    return ast;
+}
---
+++ src/engine/syntax.zig
@@ -0,0 +1,90 @@
+//! The Abstract Syntax Tree representation 
+//! of the regular expression pattern.
+
+/// Regular Expression AST Node.
+///
+/// Use pointers for recursive structures
+pub const Node = union(enum) {
+    /// Literal Unicode code point
+    Literal: Literal,
+    /// `.` Wildcard
+    AnyChar: AnyChar,
+    /// `^` Input start anchor
+    StartAnchor: StartAnchor,
+    /// `$` Input end anchor
+    EndAnchor: EndAnchor,
+    /// Character class (e.g. `[a-z]`, `[0-9]` etc.)
+    CharClass: CharClass,
+    /// Concatenation of multiple Nodes
+    Sequence: Sequence,
+    /// `|` Alternation
+    Branch: Branch,
+    /// Quantifier node (`*`, `+` or `?`) 
+    Repeat: Repeat,
+    /// Capturing group `(...)`
+    CaptureGroup: CaptureGroup,
+    /// Non-capturing group `(?:...)`
+    NonCaptureGroup: NonCaptureGroup,
+};
+
+/// Literal Unicode code point
+pub const Literal = struct {
+    value: u21,
+};
+
+/// `.` Wildcard
+pub const AnyChar = struct {};
+
+pub const StartAnchor = struct {};
+
+pub const EndAnchor = struct {};
+
+/// Inclusive character range used inside a character class.
+pub const CharRange = struct {
+    start: u21,
+    end: u21,
+};
+
+/// Character-class expression.
+///
+/// `ranges`: inclusive ranges such as `a-z` or `0-9`.
+/// 
+/// `chars`: individual literal members. 
+/// 
+/// `negated`: classes beginning with `^`, such as `[^0-9]`.
+pub const CharClass = struct {
+    ranges: []const CharRange,
+    chars: []const u21,
+    negated: bool = false,
+};
+
+/// Concatenation of child nodes that must match in order.
+pub const Sequence = struct {
+    nodes: []const *Node,
+};
+
+pub const Branch = struct {
+    left: *Node,
+    right: *Node,
+};
+
+/// Repetition Node
+/// 
+/// If `max`is `null` the repetition is without limit
+pub const Repeat = struct {
+    node: *Node,
+    min: usize,
+    max: ?usize,
+};
+
+/// Capturing group
+pub const CaptureGroup = struct {
+    /// Base-1 capture group index. 
+    /// Group 0 is reserved for the whole match
+    pos: usize,
+    node: *Node,
+};
+
+pub const NonCaptureGroup = struct {
+    node: *Node,
+};
