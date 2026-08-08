6b4725b3c732cdbdf2054e079c93c8ac5d05c9d6
oniko94 <onikolaiev94@outlook.com>
2026-08-08 16:35:23 +0300

    WIP: fix Unicode matching boundaries

--- src/unicode/rune.zig
+++ src/unicode/rune.zig
@@ -11,8 +11,9 @@ fn unicodeByteLen(value: u21) ?u4 {
     return switch(value) {
         0x0000...0x007F => 1,
         0x0080...0x07FF => 2,
-        0x0800...0xFFFF => 3,
-        0x1000...0x10FFFF => 4,
+        0x0800...0xD7FF,
+        0xE000...0xFFFF => 3,
+        0x10000...0x10FFFF => 4,
         else => null,
     };
 }
