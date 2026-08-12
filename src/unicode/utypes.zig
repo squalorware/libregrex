2df91187a8140c2b051fb47d6de7c06ec1553ef6
oniko94 <onikolaiev94@outlook.com>
2026-08-12 13:45:13 +0300

    WIP: add tests for unicode types

--- src/unicode/utypes.zig
+++ src/unicode/utypes.zig
@@ -1,3 +1,4 @@
+const testing = @import("std").testing;
 const Error = @import("types").Error;
 
 pub const SearchOrder = enum {
@@ -61,3 +62,78 @@ pub const Rune = struct {
         return self.val;
     }
 };
+
+test "RunePair.compare should compare Rune against an inclusive range" {
+    try testing.expectEqual(
+        SearchOrder.before,
+        RunePair.compare(10, 20, 9),
+    );
+
+    try testing.expectEqual(
+        SearchOrder.match,
+        RunePair.compare(10, 20, 10),
+    );
+
+    try testing.expectEqual(
+        SearchOrder.match,
+        RunePair.compare(10, 20, 15),
+    );
+
+    try testing.expectEqual(
+        SearchOrder.match,
+        RunePair.compare(10, 20, 20),
+    );
+
+    try testing.expectEqual(
+        SearchOrder.after,
+        RunePair.compare(10, 20, 21),
+    );
+}
+
+test "Rune.from should correctly determine the UTF-8 byte length of given Unicode scalar" {
+    try testing.expectEqual(
+        @as(u4, 1),
+        (try Rune.from(0x007F)).len,
+    );
+
+    try testing.expectEqual(
+        @as(u4, 2),
+        (try Rune.from(0x0080)).len,
+    );
+
+    try testing.expectEqual(
+        @as(u4, 3),
+        (try Rune.from(0x0800)).len,
+    );
+
+    try testing.expectEqual(
+        @as(u4, 4),
+        (try Rune.from(0x10000)).len,
+    );
+}
+
+test "Rune.from should return an error for invalid Unicode scalars" {
+    try testing.expectError(
+        Error.InvalidUnicode,
+        Rune.from(0xD800),
+    );
+
+    try testing.expectError(
+        Error.InvalidUnicode,
+        Rune.from(0xDFFF),
+    );
+
+    try testing.expectError(
+        Error.InvalidUnicode,
+        Rune.from(0x110000),
+    );
+}
+
+test "Rune.raw should return the original scalar value" {
+    const rune = try Rune.from(0x1F600);
+
+    try testing.expectEqual(
+        @as(u21, 0x1F600),
+        rune.raw(),
+    );
+}
